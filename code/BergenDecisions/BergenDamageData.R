0e360da2719572569515ba52f0389e56138a8f35
thordislinda <thordis@nr.no>
2016-12-17 10:04:26 +0100

    Uncertainty in Bergen

--- code/BergenDecisions/BergenDamageData.R
+++ code/BergenDecisions/BergenDamageData.R
@@ -402,17 +402,25 @@ my.slr.hist <- hist(total.slr.damage, breaks=buckets, plot=FALSE)
 my.mf.hist <- hist(total.mf.damage, breaks=buckets, plot=FALSE)
 my.dam.hist <- hist(total.dam.damage, breaks=buckets, plot=FALSE)
 plot(log(my.slr.hist$breaks[-1]), log(my.slr.hist$counts), type="h", col="#7D26CD",
-     main="", ylab="Log frequency", xlab="Total damage 2016-2100 (log million NOK)", lwd=2)
+     main="", ylab="Log frequency", xlab="Total damage 2016-2100 (million NOK)",
+     lwd=2, axes=FALSE)
+ticks <- c(50, 150, 500, 1500, 5000, 15000, 50000, 150000)
+axis(1, at = log(ticks), labels=ticks)
+axis(2)
+box()
 lines(log(my.hist$breaks[-1])+0.02, log(my.hist$counts), col="black", type="h", lwd=2)
 lines(log(my.mf.hist$breaks[-1])+0.04, log(my.mf.hist$counts), col="orange", type="h", lwd=2)
 lines(log(my.dam.hist$breaks[-1])+0.06, log(my.dam.hist$counts), col="#008B45", type="h", lwd=2)
-# abline(v=log(median(total.damage)), col="black", lty=2)
-# abline(v=log(median(total.slr.damage)), col="#7D26CD", lty=2)
-# abline(v=log(median(total.mf.damage)), col="orange", lty=2)
-# abline(v=log(median(total.dam.damage)), col="#008B45", lty=2)
-# abline(v=log(sum(yearly.median.damage)), col="gray50", lty=2)
-legend("topright", legend=c("Full uncertainty","SLR uncertainty","Effect uncertainty","Damage uncertainty"),
-       col=c("black", "#7D26CD", "orange", "#008B45"), lty=1, lwd=2)
+abline(v=log(sum(yearly.median.damage)),  col="gray50", lwd=2)
+points(log(median(total.damage)), 7.8, col="black", pch=16)
+points(log(median(total.slr.damage)),7.8,  col="#7D26CD", pch=16)
+points(log(median(total.dam.damage)),7.8,  col="#008B45", pch=16)
+points(log(sum(yearly.median.damage)),7.8,  col="gray50", pch=16)
+points(log(median(total.mf.damage)),7.8,  col="orange", pch=16)
+legend("topright",
+       legend=c("Full uncertainty","SLR uncertainty","Effect uncertainty",
+                "Damage uncertainty", "No uncertainty"),
+       col=c("black", "#7D26CD", "orange", "#008B45", "gray50"), lty=1, lwd=2)
 dev.off()
 
 #######################################################################
